cca930279e14b8d91d7099f786fb364ede169258
AlejandroMllo <AlejandroMllo@users.noreply.github.com>
2017-11-30 19:36:20 -0500

    Parses documents on user's kindle (#22)
    * Process the documents and highlights of each document and returns a
    parsed represenations of them.
    
    Known issues:  when the title of the document is too long, its ASIN number
    cannot be retrieved.
    
    * Code style changes
    
    * New style changes.
    
    * Solves some problems related with the assignment of the ASIN identifier to the
    documents.
    
    Implements the parser as a class.
    	- The constructor receives the path of the documents and optionally the name of the clippings file.
    	- Call the method getParsedFiles() to obtain the object containing, for each book:
    		- title
    		- authors
    		- asin
    		- annotations[]
    			- annotation
    				- highlight
    				- location

---
+++ kindlereader.js
@@ -0,0 +1,140 @@
+/* 
+Formats each book as:
+
+{
+books:[{
+          "title":"_title_here_"
+          "authors":["_author_lastname, _author_name_","_author2_lastname, _author2_name_"]
+          "asin":"_asin_here_"
+          "annotations":[{ 
+                          "highlight":"_highlight_here_"
+                          "location":"_location_here_"
+                          }, { 
+                          "highlight":"_highlight_here_"
+                          "location":"_location_here_"
+                          }, { 
+                          "highlight":"_highlight_here_"
+                          "location":"_location_here_"
+                          }]
+        }, {
+          "title": "_title_here_"
+          "authors": [ "_author_lastname, _author_name_" ]
+          "asin": "_asin_here_"
+          "annotations": [{ 
+                          "highlight": "_highlight_here_"
+                          "location": "_location_here_"
+                          }]
+        }]
+}
+
+let ParsedAnnotation = {
+  highlight: String,
+  location: String
+}
+
+let ParsedBook = {
+  title: String,
+  authors: [],
+  asin: String,
+  annotations: []
+}
+*/
+
+const fs = require('fs');
+const parse = require('@sole/kindle-clippings-parser').parse;
+
+class ParseKindleDirectory {
+
+  constructor(directoryPath, myClippings='My Clippings.txt') {
+    this.directoryPath = directoryPath
+    this.myClippings = ( this.directoryPath.endsWith('/') ? '' : '/' ) + myClippings
+    if (this.directoryPath.includes(this.myClippings)) {
+      this.directoryPath = this.directoryPath.substring(0, this.directoryPath.indexOf(this.myClippings))
+    }
+  }
+  
+  _parseFileFromKindle(format='utf-8') {
+    
+    const fileContents = fs.readFileSync(this.directoryPath + this.myClippings, format);
+    const parsed = parse(fileContents);
+    
+    return parsed
+  }
+
+  _getDirectories() {
+    const files = fs.readdirSync(this.directoryPath);
+    
+    const directories = new Map()
+    const underscore = '_'
+    const colon = ':'
+  
+    files.forEach(file => {
+    
+      const asinStartIndex = file.lastIndexOf(underscore)
+      let title = file.substring(0, asinStartIndex)
+      while (title.includes(underscore)) {
+        title = title.replace(underscore, colon)
+      }
+      const asin = file.substring(asinStartIndex + 1, file.lastIndexOf("."))
+      if (title && asin) {
+        directories.set(title, asin)
+      }
+    });
+  
+    return directories
+  }
+  
+  getParsedFiles() {
+  
+    const books = []
+    const directories = this._getDirectories()
+    const parsedFileFromKindle = this._parseFileFromKindle()
+  
+    parsedFileFromKindle.forEach(function(item) {
+      const { title, highlights } = item  
+      
+      //Author and Title
+      const titleIdx = title.lastIndexOf("(")
+      let simpleTitle = title.substring(0, titleIdx - 1)
+      let authors = title.substring(titleIdx)
+    
+      if (!simpleTitle && authors) {
+        simpleTitle = authors
+        authors = ""
+      }
+      
+      //Annotations
+      let annotations = []
+      highlights.forEach(function(annotation) {
+        const { metadata, text } = annotation
+    
+        const location = Math.ceil(parseInt(metadata.substring(metadata.indexOf("Location ") + 9))/150)
+        annotations.push({highlight: text, location})
+      })
+      
+      //ASIN
+      let asin = directories.get(simpleTitle)
+      if (!asin) {
+        directories.forEach(function(value, key) {
+          if (simpleTitle.includes(key)) {
+            asin = value
+          } else if (key.includes(simpleTitle)) {
+            asin = value
+            simpleTitle = key
+          }
+        })
+      }
+
+      //Book
+      books.push({title: simpleTitle, authors, asin: asin, annotations})
+    });
+    
+    return books
+  }
+}
+
+/* 
+const kindleFile = new ParseKindleDirectory('./samples')
+const books = kindleFile.getParsedFiles()
+console.log(books) 
+*/
