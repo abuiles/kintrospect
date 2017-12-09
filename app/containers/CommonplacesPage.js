90b98eeb8aa1d37542af01042fec91d274bbfd47
Adolfo Builes <builes.adolfo@gmail.com>
2017-12-09 10:47:43 -0500

    Add link to commonplace route from commonplace books.

--- app/containers/CommonplacesPage.js
+++ app/containers/CommonplacesPage.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react'
 import { observer, inject } from 'mobx-react'
+import { Link } from 'react-router-dom'
 
 @inject('booksStore')
 @inject('amazonStore')
@@ -28,6 +29,9 @@ export default class CommonplacesPage extends Component {
       <div className="w-100">
         <div className="h-100 flex flex-column ph3 bl b--near-white bg-light-gray relative">
           <h1>Your Commonplace Books</h1>
+          <Link className="no-underline mr4" to="/commonplace-book" >
+            Commonplace Page
+          </Link>
         </div>
       </div>
     );
