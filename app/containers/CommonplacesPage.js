0abcc8a40a1c1f94bbc2bd7873ff514a2dc6310b
Adolfo Builes <builes.adolfo@gmail.com>
2017-12-09 10:11:31 -0500

    Add container for commonplaces.

---
+++ app/containers/CommonplacesPage.js
@@ -0,0 +1,35 @@
+// @flow
+import React, { Component } from 'react'
+import { observer, inject } from 'mobx-react'
+
+@inject('booksStore')
+@inject('amazonStore')
+@inject('analytics')
+@observer
+export default class CommonplacesPage extends Component {
+  props: {
+    booksStore: BookStore,
+    amazonStore: AmazonStore,
+    analytics: any
+  }
+
+  componentDidMount() {
+    const { analytics } = this.props
+    analytics.pageview(
+      'https://app.kintrospect.com',
+      '/commonplace-books',
+      'Commonplace Books',
+      analytics._machineID
+    )
+  }
+
+  render() {
+    return (
+      <div className="w-100">
+        <div className="h-100 flex flex-column ph3 bl b--near-white bg-light-gray relative">
+          <h1>Your Commonplace Books</h1>
+        </div>
+      </div>
+    );
+  }
+}
