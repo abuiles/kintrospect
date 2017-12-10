b870c5dd2453738e910ef04e50a843507ca64d41
Adolfo Builes <builes.adolfo@gmail.com>
2017-12-09 19:45:35 -0500

    Show all highlights in the commonplace :)

--- app/components/Commonplace.js
+++ app/components/Commonplace.js
@@ -4,33 +4,71 @@ import { observer, inject } from 'mobx-react'
 import SearchInput, { createFilter } from 'react-search-input'
 
 import NotesEditor from './NotesEditor';
+import AnnotationView from './Annotation';
 
 import { Commonplace } from '../stores/Commonplace'
 import NoteStore from '../stores/Note'
+import RootStore from '../stores/Root'
 
-@inject('notesStore')
+const KEYS_TO_FILTERS = ['highlight', 'name']
+
+@inject('notesStore', 'rootStore')
 @observer
 export default class CommonplaceView extends Component {
   state: {
-    searchTerm: string
+    searchTerm: string,
+    selectedAnnotation: any
   }
 
   state = {
-    searchTerm: ''
+    searchTerm: '',
+    selectedAnnotation: null
   }
 
   props: {
     commonplace: Commonplace,
-    notesStore: NoteStore
+    notesStore: NoteStore,
+    rootStore: RootStore
   }
 
   searchUpdated(term: string) {
     this.setState({ searchTerm: term })
   }
 
+  selectAnnotation(annotation: any) {
+    const { selectedAnnotation } = this.state
+
+    this.setState({
+      selectedAnnotation: selectedAnnotation === annotation ? null : annotation
+    })
+  }
+
+
   render() {
-    const { commonplace, notesStore } = this.props
+    const { commonplace, notesStore, rootStore } = this.props
+    const { searchTerm, selectedAnnotation } = this.state
     const { title } = commonplace
+
+    const annotations = rootStore.allAnnotations
+
+    const filteredAnnotations = annotations.filter(createFilter(searchTerm, KEYS_TO_FILTERS))
+
+    const AnnotationsList = (({ list }) => (
+      <div className="pt1">
+        {list.map((annotation) =>
+          <div id={annotation.linkId} key={annotation.uniqueKey}>
+            <AnnotationView
+              annotation={annotation}
+              asin={annotation.asin}
+              isKindleBook={annotation.isKindleBook}
+              isHighlighted={annotation === selectedAnnotation}
+              selectAnnotation={(selected) => this.selectAnnotation(selected)}
+            />
+          </div>
+        )}
+      </div>
+    ))
+
 
     return (
       <div className="flex w-100 h-100">
@@ -42,7 +80,7 @@ export default class CommonplaceView extends Component {
             <SearchInput className="search-input w-100" onChange={(term) => this.searchUpdated(term)} />
           </div>
           <div className="overflow-y-auto h-100 ph3">
-            <p>highlights come here</p>
+            {annotations.length ? <AnnotationsList list={filteredAnnotations} /> : !isRunning && <h3>Create some highlights first</h3>}
           </div>
         </div>
 
