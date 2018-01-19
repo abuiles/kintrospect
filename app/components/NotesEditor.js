633abab69f930565daf1ec4abb0fac9c7c19eaf6
Adolfo Builes <builes.adolfo@gmail.com>
2018-01-19 10:55:17 -0800

    Show link form.

--- app/components/NotesEditor.js
+++ app/components/NotesEditor.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import { Container, Editor, MarkupButton, SectionButton, LinkButton } from 'react-mobiledoc-editor';
+import { UI } from 'mobiledoc-kit';
 import { observer, inject } from 'mobx-react'
 import { DropTarget } from 'react-dnd';
 import { Debounce } from 'react-throttle';
@@ -37,6 +38,17 @@ const boxTarget = {
 @inject('notesStore', 'analytics')
 @observer
 class NotesEditor extends React.Component {
+  state: {
+    currentLink: string,
+    linkPortalStyle: any,
+    range: any
+  }
+
+  state = {
+    currentLink: '',
+    linkPortalStyle: null
+  }
+
   componentWillUnmount() {
     const { notesStore } = this.props
     notesStore.setEditor(null)
@@ -77,6 +89,60 @@ class NotesEditor extends React.Component {
     analytics.event('Notes', 'downloaded', { evLabel: book.asin, clientID: analytics._machineID })
   }
 
+  openLinkPortal(e) {
+    const { mobiledocEditor } = this.props.notesStore
+    const editor = mobiledocEditor
+
+    if (!editor.hasCursor()) {
+      return;
+    }
+
+    if (editor.hasActiveMarkup('a')) {
+      editor.toggleMarkup('a');
+    } else {
+      this.setLinkPortalPosition()
+    }
+  }
+
+  setLinkPortalPosition(opening = true) {
+    if (!opening) {
+      return this.setState({
+        currentLink: '',
+        linkPortalStyle: null
+      })
+    }
+
+    const position =  window.getSelection().getRangeAt(0).getBoundingClientRect()
+
+    const linkStyle = {
+      position: 'absolute',
+      left: position.left - 10,
+      top: position.bottom + 18
+    };
+
+    const { mobiledocEditor } = this.props.notesStore
+    const editor = mobiledocEditor
+
+    this.setState({
+      linkPortalStyle: linkStyle,
+      range: editor.range
+    })
+  }
+
+  setLink() {
+    const { currentLink, range } = this.state
+
+    const { mobiledocEditor } = this.props.notesStore
+    const editor = mobiledocEditor
+
+    editor.run(function (postEditor) {
+      const markup = postEditor.builder.createMarkup('a', { href: currentLink });
+      postEditor.addMarkupToRange(range, markup);
+    });
+
+    this.setLinkPortalPosition(false)
+  }
+
   didCreateEditor(editor) {
     const { notesStore } = this.props
     notesStore.setEditor(editor)
@@ -88,14 +154,30 @@ class NotesEditor extends React.Component {
 
     const doc = notesStore.findNotes(book)
 
+    const { linkPortalStyle, currentLink } = this.state
+
     let backgroundColor = ''
 
     if (isActive) {
       backgroundColor = 'bg-yellow'
     }
 
+    const LinkForm = (
+      <div className="bg-white ba pa1" style={linkPortalStyle}>
+        <form onSubmit={(event) => event.preventDefault()} acceptCharset="utf-8">
+          <label className="lh-copy f6 mb1" htmlFor="link">Link</label>
+          <input className="w-100 mb3 pa2 ba b--gray" name="link" type="text" value={currentLink} onChange={(event) => this.setState({ currentLink: event.target.value })} />
+            <div className="tc">
+              <button className="btn mh2" onClick={() => this.setLink()}>Apply</button>
+              <button className="btn mh2" onClick={() => this.setLinkPortalPosition(false)}>Cancel</button>
+            </div>
+        </form>
+      </div>
+    )
+
     return connectDropTarget(
       <div className="h-100 pa3">
+        {linkPortalStyle && LinkForm}
         <Debounce time="500" handler="onChange">
           <Container
             className="flex flex-column w-100 h-100 bg-white pa2"
@@ -119,25 +201,12 @@ class NotesEditor extends React.Component {
                   </MarkupButton>
                 </li>
                 <li className="dib mr4">
-                  <PortalWithState closeOnOutsideClick closeOnEsc>
-                    {({ openPortal, closePortal, portal }) => (
-                      <div>
-                        <button
-                          className="bn pa0 bg-inherit lh-solid"
-                          onClick={openPortal}
-                          >
-                          <i className="silver fa fa-link" aria-hidden="true" />
-                        </button>
-                        {portal(
-                          <p>
-                            This is more advanced Portal. It handles its own state.{' '}
-                            <button onClick={closePortal}>Close me!</button>, hit ESC or
-                          click outside of me.
-                            </p>
-                        )}
-                      </div>
-                    )}
-                  </PortalWithState>
+                  <button
+                    className="bn pa0 bg-inherit lh-solid"
+                    onClick={(e) => this.openLinkPortal(e)}
+                  >
+                    <i className="silver fa fa-link" aria-hidden="true" />
+                  </button>
                 </li>
                 <li className="dib mr4">
                   <SectionButton tag="h1" className="bn pa0 bg-inherit lh-solid">
