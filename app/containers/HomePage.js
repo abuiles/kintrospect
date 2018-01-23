997b75977d7e94d7332e80f72a1f7cd9add67fd3
Adolfo Builes <builes.adolfo@gmail.com>
2018-01-23 17:53:31 -0500

    Use shell open for downloads.

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -5,6 +5,7 @@ import { observer, inject } from 'mobx-react'
 import Home from '../components/Home'
 import BookStore from '../stores/Book'
 import AmazonStore from '../stores/Amazon'
+const { shell } = require('electron').remote
 
 // https://wietse.loves.engineering/using-flowtype-with-decorators-in-react-af4fe69e66d6
 @inject('booksStore')
@@ -23,6 +24,11 @@ export default class HomePage extends Component {
     analytics.pageview('https://app.kintrospect.com', '/home', 'Home', analytics._machineID)
   }
 
+  openDownloads(event) {
+    event.preventDefault();
+    shell.openExternal('https://kintrospect.com/downloads.html')
+  }
+
   render() {
     const { booksStore, amazonStore } = this.props
     const { kindleSignedIn, hasWebview, isRunning } = amazonStore
@@ -35,7 +41,7 @@ export default class HomePage extends Component {
         <div className="vh-100 tc bg-blue">
           <div className="paragraph center mw-100 white pv4">
             <h1 className="f1 lh-title"><i className="fa fa-rocket" aria-hidden="true" /> Woohoo! An improved version of Kintrospect is now available for download at:</h1>
-            <a className="f2 white" href="https://kintrospect.com/downloads.html" target="_blank" rel="noopener noreferrer">https://kintrospect.com/downloads.html</a>
+            <a className="f2 white" onClick={(e) => this.openDownloads(e) } href="https://kintrospect.com/downloads.html" target="_blank" rel="noopener noreferrer">https://kintrospect.com/downloads.html</a>
           </div>
         </div>
       )
